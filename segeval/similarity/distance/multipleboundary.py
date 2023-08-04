984a0bcc01ea534f288e4400809582ff535277c1
David Nicholson <nickledave@users.noreply.github.com>
2023-08-03 22:00:26 -0400

    Further revise docstring + type hints in segeval/similarity/distance/multipleboundary.py

--- segeval/similarity/distance/multipleboundary.py
+++ segeval/similarity/distance/multipleboundary.py
@@ -3,7 +3,8 @@ Multiple-boundary edit distance.
 
 .. moduleauthor:: Chris Fournier <chris.m.fournier@gmail.com>
 '''
-from __future__ import absolute_import, division
+from __future__ import absolute_import, annotations, division
+
 from itertools import permutations
 from collections import namedtuple
 
@@ -14,7 +15,7 @@ Transposition = namedtuple('Transposition', 'start end type')
 Difference = namedtuple('Difference', 'sim a_b b_a')
 
 
-def __additions_substitutions__(d, a, b):
+def additions_substitutions(d: set, a: set, b: set):
     '''
     Compute the number of additions and substitutions for a given pair of
     boundary string positions using:
@@ -25,6 +26,16 @@ def __additions_substitutions__(d, a, b):
     :type d: set
     :type a: set
     :type b: set
+
+    Examples
+    --------
+    >>> a_i = set([1, 2, 3, 4])
+    >>> b_i = set([1, 6])
+    >>> a = a_i - b_i
+    >>> b = b_i - a_i
+    >>> d = a_i ^ b_i
+    >>> segeval.similarity.distance.multipleboundary.additions_substitutions(d, a, b)
+    (2, 1)
     '''
 
     additions = abs(len(a) - len(b))
@@ -42,6 +53,16 @@ def additions_substitutions_sets(d: set, a: set, b: set) -> tuple[list, set]:
     :type d: set
     :type a: set
     :type b: set
+
+    Examples
+    --------
+    >>> a_i = set([1, 2, 3, 4])
+    >>> b_i = set([1, 6])
+    >>> a = a_i - b_i
+    >>> b = b_i - a_i
+    >>> d = a_i ^ b_i
+    >>> segeval.similarity.distance.multipleboundary.additions_substitutions_sets(d, a, b)
+    ([(2, 'a'), (3, 'a')], set([(4, 6)]))
     '''
 
     substitutions = list()
@@ -72,7 +93,7 @@ def additions_substitutions_sets(d: set, a: set, b: set) -> tuple[list, set]:
     return added, set([Substitution(a_i, b_i) for a_i, b_i in set(substitutions)])
 
 
-def __has_substitutions__(i, j, d, options_set):
+def __has_substitutions__(i: int, j: int, d, options_set):
     '''
     Determine whether two substitutions are present involving the boundary 'd'
     at the positions 'i' and 'j'.
@@ -82,15 +103,15 @@ def __has_substitutions__(i, j, d, options_set):
     if i in options_set and d in options_set[i][0] and j in options_set and d in options_set[j][0]:
         d_i, a_i, b_i = options_set[i]
         d_j, a_j, b_j = options_set[j]
-        if __additions_substitutions__(d_i, a_i, b_i)[1] > 0 and \
-           __additions_substitutions__(d_j, a_j, b_j)[1] > 0:
+        if (additions_substitutions(d_i, a_i, b_i)[1] > 0 and
+            additions_substitutions(d_j, a_j, b_j)[1] > 0):
             present = True
     return present
 
 
-def __overlaps_existing__(i, j, d, options_transp):
+def __overlaps_existing__(i: int, j: int, d, options_transp):
     '''
-    Determine whether a  transposition would overlap another that has been
+    Determine whether a transposition would overlap another that has been
     previously identified involving the boundary 'd' at the positions 'i' and
     'j'.
     '''
@@ -107,41 +128,51 @@ def __overlaps_existing__(i, j, d, options_transp):
     return check_position(i) or check_position(j)
 
 
-def find_transpositions(boundary_string_a, boundary_string_b, n, options_set):
+def find_transpositions(boundary_string_a: list[frozenset], boundary_string_b: list[frozenset],
+                        n_t: list[int], options_set: dict[int: Difference]) -> list[Transposition]:
     '''Identify all non-overlapping minimal transpositions between two boundary
     strings while removing the additions/deletions and substitutions that they
     would overlap from the set of potential additions/deletions and
     substitutions.
+
+    Algorithm 4.3 from Fournier 2013.
     '''
 
-    options_transp = dict()
+    options_transp: dict[Transposition] = dict()  # stored transitions :math:`O_T` that we check
     transpositions = list()
-    for n_i in sorted(n):
+    for n_i in sorted(n_t):
         n_i = n_i - 1
         for i in range(0, len(boundary_string_a) - n_i):
             j = i + n_i
+
             # Select edge sets
-            a_i = boundary_string_a[i]
-            a_j = boundary_string_a[j]
-            b_i = boundary_string_b[i]
-            b_j = boundary_string_b[j]
+            a_i: frozenset = boundary_string_a[i]
+            a_j: frozenset = boundary_string_a[j]
+            b_i: frozenset = boundary_string_b[i]
+            b_j: frozenset = boundary_string_b[j]
+
             # Compute symmetric differences
             diff_i = a_i ^ b_i
             diff_j = a_j ^ b_j
             diff_a = a_i ^ a_j
             diff_b = b_i ^ b_j
+
             # Detect potential transposition
             t_p = diff_i & diff_j & diff_a & diff_b
+
             # Apply each transposition found by boundary type
             for d in t_p:
                 # Create transposition representation
                 option_transp = Transposition(i, j, d)
+
                 # Check to see that it does not overlap an existing
                 # transposition and that 2 substitutions are not removed
-                if not __overlaps_existing__(i, j, d, options_transp) and \
-                   not __has_substitutions__(i, j, d, options_set):
+                if (not __overlaps_existing__(i, j, d, options_transp) and
+                    not __has_substitutions__(i, j, d, options_set)):
+
                     # Add
                     transpositions.append(option_transp)
+
                     # Record positions covered
                     if i not in options_transp:
                         options_transp[i] = list()
@@ -149,6 +180,7 @@ def find_transpositions(boundary_string_a, boundary_string_b, n, options_set):
                         options_transp[j] = list()
                     options_transp[i].append(option_transp)
                     options_transp[j].append(option_transp)
+
                     # Removing potential set errors that overlap
                     options_set[i][0].discard(d)
                     options_set[i][1].discard(d)
@@ -156,11 +188,17 @@ def find_transpositions(boundary_string_a, boundary_string_b, n, options_set):
                     options_set[j][0].discard(d)
                     options_set[j][1].discard(d)
                     options_set[j][2].discard(d)
+
     return transpositions
 
 
-def optional_set_edits(boundary_string_a, boundary_string_b):
-    '''Identify all potential additions/deletions and substitutions.
+def optional_set_edits(
+        boundary_string_a: list[frozenset], boundary_string_b: list[frozenset]
+) -> dict[int: Difference]:
+    '''Identify all potential additions/deletions and substitutions,
+    the optional set edits :math:`O_S`.
+
+    Algorithm 4.2 from Fournier 2013.
     '''
 
     options_set = dict()
@@ -176,7 +214,8 @@ def optional_set_edits(boundary_string_a, boundary_string_b):
 
 
 
-def boundary_edit_distance(boundary_string_a, boundary_string_b, n_t=2):
+def boundary_edit_distance(boundary_string_a: list[frozenset], boundary_string_b: list[frozenset],
+                           n_t: int = 2) -> tuple[list, list, list]:
     '''Computes boundary edit distance between two boundary strings.
 
     Identifies the minimum set of additions, substitutions, and transpositions
@@ -184,6 +223,8 @@ def boundary_edit_distance(boundary_string_a, boundary_string_b, n_t=2):
     of transpositions spanning lengths ``n_t``.
 
     Returns a list of Addition, Substitution, and Transposition edit sets.
+
+    Algorithm 4.1 from Fournier 2013.
 
     :param boundary_string_a:
         Boundary string to compare; produced by :func:`boundary_string_from_masses`
